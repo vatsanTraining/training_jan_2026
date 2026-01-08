a296eafed91aba38b1145d7e8c607a6081f5a88d
srivatsan <vatsan_k@hotmail.com>
2026-01-08 11:57:05 +0530

    Day Four Session One Completion

--- java_nine_seventeen_08/src/com/example/demo/Application.java
+++ java_nine_seventeen_08/src/com/example/demo/Application.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import com.example.demo.dto.Invoice;
 import com.example.demo.model.AutoPolicy;
@@ -28,6 +29,20 @@ public static void recordDemo() {
 		System.out.println("*******  Set ********");
 		
 		service.getWithSet().forEach(System.out::println);
+		
+		
+		System.out.println("######  Take While ######");
+		
+		service.getWithSet().stream().takeWhile(e -> e.amount()<7000).forEach(System.out::println);
+
+//		System.out.println("Using List");
+//		list.stream().takeWhile(e -> e.amount()<5000).forEach(System.out::println);
+//		
+        System.out.println("&&&&&& Drop While &&&&&&&");
+		
+		service.getWithSet().stream().dropWhile(e -> e.amount()<5000).forEach(System.out::println);
+
+		
 	}
 
 	public static void main(String[] args) {
@@ -39,6 +54,27 @@ public static void recordDemo() {
 		InsurancePolicy policy = new AutoPolicy();
 		
 		System.out.println(policy.calculatePremium());
+		
+		Predicate<Integer> checkAge = (age) -> age>18;
+		
+		
+		System.out.println("is Age Greater Than 18 =>" +checkAge.test(25));
+		
+		System.out.println(checkAge.getClass().getName());
+		
+		String invoiceAsJson = """ 
+				
+				{
+				   "invoiceNumber":101,
+				   "customerName":"ramesh",
+				   "InvoiceDate":2024-10-10,
+				   "amount":5000
+				}
+				
+				""";
+		
+		
+		System.out.println(invoiceAsJson);
 	}
 
 	
