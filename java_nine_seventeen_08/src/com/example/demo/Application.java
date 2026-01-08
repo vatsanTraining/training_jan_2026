3b0e83b0aee86ce5e5c7c7259f4de7b713a417f8
srivatsan <vatsan_k@hotmail.com>
2026-01-08 11:08:44 +0530

    Update

--- java_nine_seventeen_08/src/com/example/demo/Application.java
+++ java_nine_seventeen_08/src/com/example/demo/Application.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.example.demo.dto.Invoice;
 import com.example.demo.repo.InvoiceRepository;
+import com.example.demo.services.InvoiceService;
 
 public class Application {
 
@@ -13,10 +14,20 @@ public class Application {
 	
 		var repo = new InvoiceRepository();
 		
-		List<Invoice> list = repo.findAll();
+
+		var service = new InvoiceService(repo);
 		
+		List<Invoice> list = service.findAll();
+		
+		System.out.println("====== List =========");
 		
 		list.forEach(System.out::println);
+	
+		
+		System.out.println("*******  Set ********");
+		
+		service.getWithSet().forEach(System.out::println);
+		
 		
 	}
 
