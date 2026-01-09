745b6268a46581dffb391bec29220279a4b97ba2
srivatsan <vatsan_k@hotmail.com>
2026-01-09 11:46:45 +0530

    Update One

---
+++ spring_core_10/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.training.model.Player;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		
+		  BeanFactory ctx = new ClassPathXmlApplicationContext("bean.xml");
+		  
+		  
+		  Player sachin = ctx.getBean(Player.class);
+		  
+		  System.out.println(sachin);
+		  
+		  ((ConfigurableApplicationContext)ctx).close();
+		  
+//		  Player sachin = new Player();
+//		  sachin.setPlayerId(1020);
+//		  sachin.setJerseryNumber(10);
+//		  sachin.setPlayerName("Sachin");
+	}
+
+}
