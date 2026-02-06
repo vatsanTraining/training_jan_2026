5321979d1603d9642fbe45601b9c3cb579ee91c7
srivatsan <vatsan_k@hotmail.com>
2026-02-06 17:41:48 +0530

    Jenkins demo added

--- spring_boot_jdbc_13/src/main/java/com/example/demo/SpringBootJdbc13Application.java
+++ spring_boot_jdbc_13/src/main/java/com/example/demo/SpringBootJdbc13Application.java
@@ -9,11 +9,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.example.demo.dto.Invoice;
 import com.example.demo.services.InvoiceService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class SpringBootJdbc13Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootJdbc13Application.class);
+
 	public static void main(String[] args) {
+		
+	    
+		logger.info("application started");
+		
+		logger.info("--- SPLUNK IS READY. APPLICATION IS SENDING LOGS ---");
+		
 	ConfigurableApplicationContext	 ctx=SpringApplication.run(SpringBootJdbc13Application.class, args);
 	
 	
@@ -27,6 +38,13 @@ System.out.println(service.findByInvoiceNumber(103));
   service.findAll().forEach(System.out::println);
 
   
+  // Force a pause here to let HEC flush the logs to Splunk
+  try {
+	Thread.sleep(5000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+} // Wait for 5 seconds
  // ctx.close();
   
 	}
--- spring_boot_jdbc_13/src/main/java/com/example/demo/services/InvoiceService.java
+++ spring_boot_jdbc_13/src/main/java/com/example/demo/services/InvoiceService.java
@@ -2,8 +2,11 @@ package com.example.demo.services;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.SpringBootJdbc13Application;
 import com.example.demo.dto.Invoice;
 import com.example.demo.ifaces.MyDao;
 
@@ -11,11 +14,14 @@ import com.example.demo.ifaces.MyDao;
 public class InvoiceService {
 
 	
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootJdbc13Application.class);
+
 	private MyDao<Invoice> dao;
 
 	public InvoiceService(MyDao<Invoice> dao) {
 		super();
 		this.dao = dao;
+		logger.info("Service constructed");
 	}
 	
 	public boolean add(Invoice inv) {
@@ -23,11 +29,14 @@ public class InvoiceService {
 	}
 	
 	public List<Invoice> findAll(){
+		logger.info("Find All Called");
 		return this.dao.findAll();
 	}
 	
 	public Invoice findByInvoiceNumber(int invoiceNumber) {
 		
+		logger.info("Find All Called");
+
 		return this.dao.findById(invoiceNumber);
 		
 	}
